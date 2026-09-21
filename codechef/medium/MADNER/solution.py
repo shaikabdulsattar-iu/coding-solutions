860ebb6303a93714851c471f1e61bddec3e7e8ab
Shaikabdul sattar <shaikabdulsattar663@gmail.com>
2026-09-21 21:32:50 +0530

    ✅ MADNER | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-21

--- codechef/medium/MADNER/solution.py
+++ codechef/medium/MADNER/solution.py
@@ -1,3 +1,13 @@
 class Solution:
     def findMaximumPairs(self, students: str) -> int:
-        
+        count = 0
+        i = 0
+
+        while i < len(students) - 1:
+            if students[i] != students[i + 1]:
+                count += 1
+                i += 2
+            else:
+                i += 1
+
+        return count
