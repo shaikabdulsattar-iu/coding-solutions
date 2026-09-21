95a81b634b0c782298178a066bafbc43383859a0
Shaikabdul sattar <shaikabdulsattar663@gmail.com>
2026-09-21 07:33:45 +0530

    ✅ AMR15A - Rating 533 | Easy | Python | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ codechef/easy/AMR15A---Rating-533/solution.py
@@ -0,0 +1,11 @@
+# cook your dish here
+n  = int(input())
+l1 = list(map(int,input().split()))
+c = 0
+for i in l1:
+    if i % 2 == 0:
+        c += 1
+if c > n - c:
+    print("ready for battle")
+else:
+    print("not ready")
